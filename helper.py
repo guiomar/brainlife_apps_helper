ad40b2ae1b67c5400bce27bbc0ac68a785ea7425
Guiomar Niso <guiomar.niso@ctb.upm.es>
2021-12-15 13:29:15 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -174,15 +174,18 @@ def read_optional_files(config, out_dir_name):
             if channels_file is not None:
                 shutil.copy2(channels_file, os.path.join(out_dir_name, 'channels.tsv'))
                 os.chmod(os.path.join(out_dir_name, 'channels.tsv'), 775) # chmod u=rwx,g=rwx,o=rx  | chmod 775 |
+                print('hola1')
         else:
             shutil.copy2(channels_file_override, os.path.join(out_dir_name, 'channels.tsv'))
             os.chmod(os.path.join(out_dir_name, 'channels.tsv'), 775) # chmod u=rwx,g=rwx,o=rx  | chmod 775 |
+            print('hola2')
             channels_file = channels_file_override
     else:
         if channels_file is not None:
             shutil.copy2(channels_file, os.path.join(out_dir_name, 'channels.tsv'))
             os.chmod(os.path.join(out_dir_name, 'channels.tsv'), 775) # chmod u=rwx,g=rwx,o=rx  | chmod 775 |       
-        
+            print('hola3')
+     
     # Read the events file
     if "events_override" in config.keys():
         events_file_override = config.pop('events_override')
